6dad27ed8e870b55c5f5cfd50cc4ab2f379c9129
HappyFeed <alejomam2011@gmail.com>
2021-12-03 12:25:33 -0500

    Admi dashboard done

---
+++ src/ui/AdmiController.java
@@ -0,0 +1,33 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+
+public class AdmiController {
+
+    @FXML
+    private Button buttonDelete;
+
+    @FXML
+    private Button buttonSetPassword;
+
+    @FXML
+    private ComboBox<?> comboBoxUsers;
+
+    @FXML
+    private ImageView imageAdmi;
+
+    @FXML
+    void deleteUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setUserPassword(ActionEvent event) {
+
+    }
+
+}
